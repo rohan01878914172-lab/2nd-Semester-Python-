fbf05525f58544f681e34ac2130a2ac12896b311
rohan01878914172-lab <rohan01878914172@gmail.com>
2026-07-03 20:14:14 +0600

    Add files via upload

---
+++ Job1.py
@@ -0,0 +1,42 @@
+print("Hello World")
+
+print("Bangladesh Technical Education Board")
+
+def show():
+    print("Bangladesh Teachnical Education Board")
+show()
+
+a=int(input("Enter the First Number="))
+b=int(input("Enter the Second Number="))
+sum=a+b
+dif=a-b
+mul=a*b
+div=a/b
+print("a+b",sum)
+print("a-b",dif)
+print("a*b",mul)
+print("a/b",div)
+
+import math
+r=float(input("Enter Radius"))
+area=math.pi*r*r
+print("Circle Area=",area)
+
+height=float(input("Enter hight="))
+width=float(input("Enter width="))
+area=0.5*height*width
+print("Triangle Area=",area)
+
+lerght=float(input("Enter lenght="))
+width=float(input("Enter width="))
+area=lerght*width
+print("Rectangle Area=",area)
+
+celsius=float(input("Enter the Celsius Temperature="))
+fahrenheit=(celsius*1.8)+32
+print("celsius to Fahrenheit Temperature=",fahrenheit)
+
+fahrenhit=float(input("Enter the Fahrenheit Temperature=?"))
+celsius=(fahrenheit-32)/1.8
+print("Fahrenheit to Celsius Temperature=",celsius)
+
